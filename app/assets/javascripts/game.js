48480c5f8a7b9f1f1282dacb56dbcc289053d650
melki <melchior.toldi@gmail.com>
2013-05-24 16:09:19 +0200

    ça prend forme

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -11,29 +11,51 @@ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext("2d");
 var width = canvas.width;
 var height = canvas.height;
-var lvl = 1;
+var lvl = 0;
 var baseSpeed = 10;
-var cube;
+var cube,nbLvl,inJump,boom=0;
 var c1 = new Array();
 var c2 = new Array();
 var speed = new Array();
+var nbObstacles = new Array();
+var obstacles = new Array();
+var gravity = new Array();
 var sol = new Array();
 var ciel = new Array();
 var creation=0;
-
+var stopTheGame = 0;
+var nbTry = 0;
+var frame = 0;
+var debris = new Array();
  
-$.getJSON('lvl.json', function(data) {
-    var nbLvl = data.nbLvl
-    for (var i = 1; i <= nbLvl; i++) {
+$.getJSON('lvl.json', function(data) 
+{
+    nbLvl = data.nbLvl;
+    for (var i = 0; i < nbLvl; i++) 
+    {
        
         c1[i] = data.lvl[i].c1;
         c2[i] = data.lvl[i].c2;
+        gravity[i] = data.lvl[i].gravity;
         sol[i] = data.lvl[i].sol;
         ciel[i] = data.lvl[i].ciel;
         speed[i] = data.lvl[i].speed;
-
-       
-         };
+        nbObstacles[i] = data.lvl[i].nbObstacles;
+        if(nbObstacles[i] != 0) 
+        {
+            obstacles[i]=new Array();
+            for (var j = 0; j < nbObstacles; j++) 
+            {
+                obstacles[i][j]=
+                {
+                    "posX":data.lvl[i].obstacles[j].posX,
+                    "posY":data.lvl[i].obstacles[j].posY,
+                    "width":data.lvl[i].obstacles[j].width,
+                    "height":data.lvl[i].obstacles[j].height
+                };
+            }   
+        }
+    };
     animate();
 });
 
@@ -41,22 +63,109 @@ $.getJSON('lvl.json', function(data) {
 
 
 function animate() 
-{
-    if(creation!=1)
-        {
-            creation=1;
-            createCube(sol[lvl],c1[lvl],speed[lvl]);
-        }
+{   
     requestAnimFrame(animate);
-    clearCanvas(ctx);
-    moveCube();
-    drawBackground(ciel[lvl],sol[lvl],c1[lvl],c2[lvl]);
-    drawCube(c2[lvl]);
+    if(stopTheGame==1)
+    {    
+        if (boom==0)
+        {
+            if(creation!=1)
+                {
+                    creation=1;
+                    createCube(sol[lvl],c1[lvl],speed[lvl]);
+                }
+            clearCanvas(ctx);
+            moveCube();
+            drawBackground(ciel[lvl],sol[lvl],c1[lvl],c2[lvl]);
+            drawCube(c2[lvl]);
+            if (nbObstacles[lvl]!=0)
+            {
+                drawObstacles(obstacles[lvl],nbObstacles[lvl]);
+                collisionsObstacles(obstacles[lvl],nbObstacles[lvl]);
+            }
+            collisionsCiel();
+            testArrive();
+            infos();
+        }
+        if (boom==1)
+        {
+            clearCanvas(ctx);
+            drawBackground(ciel[lvl],sol[lvl],c1[lvl],c2[lvl]);
+            explosion();
+            infos();
+            if (frame>20) {boom=0}
+        }
+    }    
+}
 
+function collisionsCiel()
+{
+    if(cube.posY<=ciel[lvl])
+    {
+        avantExplosion();
+    }
+}
+
+function collisionsObstacles(obstacles,nb)
+{
+    for (i = 0; i < nb; i++) 
+    {
+        if (!((cube.posX>=(obstacles[i].posX+obstacles[i].width) || ((cube.posY + 20 )<=obstacles[i].posY) || (cube.posY>=(obstacles[i].posY+obstacles[i].height)) || ((cube.posX+20)<=obstacles[i].posX))))
+        {
+           avantExplosion();
+        }
+    }
+}
+
+function avantExplosion()
+{
+        nbTry+=1;
+        boom=1;
+        creation=0;
+        debris[0]={"posX":cube.posX,"posY":cube.posY};
+        debris[1]={"posX":cube.posX,"posY":cube.posY+5};
+        debris[2]={"posX":cube.posX,"posY":cube.posY+10};
+        debris[3]={"posX":cube.posX,"posY":cube.posY+15};
+        debris[4]={"posX":cube.posX+5,"posY":cube.posY};
+        debris[5]={"posX":cube.posX+5,"posY":cube.posY+5};
+        debris[6]={"posX":cube.posX+5,"posY":cube.posY+10};
+        debris[7]={"posX":cube.posX+5,"posY":cube.posY+15};
+        debris[8]={"posX":cube.posX+10,"posY":cube.posY};
+        debris[9]={"posX":cube.posX+10,"posY":cube.posY+5};
+        debris[10]={"posX":cube.posX+10,"posY":cube.posY+10};
+        debris[11]={"posX":cube.posX+10,"posY":cube.posY+15};
+        debris[12]={"posX":cube.posX+15,"posY":cube.posY};
+        debris[13]={"posX":cube.posX+15,"posY":cube.posY+5};
+        debris[14]={"posX":cube.posX+15,"posY":cube.posY+10};
+        debris[15]={"posX":cube.posX+15,"posY":cube.posY+15};
+    
+}
+
+function explosion()
+{
+    frame+=1;
+    for (var j = 0; j < 16; j++) {
+        debris[j].posX+=Math.random()*20-10;
+        debris[j].posY+=Math.random()*20-10;
+    };
+    for (j = 0; j < 16; j++) {
+        ctx.fillStyle = c2;
+        ctx.fillRect(debris[j].posX,debris[j].posY,5,5);
+    };
+       
+}
+
+function infos()
+{
+    ctx.fillStyle = c1[lvl];
+    ctx.font = "20px Tahoma,Calibri,Geneva,Arial";
+    ctx.fillText(' || tries : '+ nbTry, 20, 20);
 }
 
 function createCube(sol,c1,speed) 
 {
+    frame=0;
+    inJump=0;    
     cube = {
             "color": c1,
             "posX": 0,
@@ -75,26 +184,107 @@ function drawBackground(ciel,sol,c1,c2)
     ctx.fillRect(0,sol,width,height-sol);
 
 }
+function drawObstacles(obstacles,nb)
+{
+    for (i = 0; i < nb; i++)
+    {
+           
+        ctx.fillStyle = c2;
+        ctx.fillRect(obstacles[i].posX,obstacles[i].posY,obstacles[i].width,obstacles[i].height);
+         
+    }
+}
 
 function moveCube()
 {
-    cube.posX=cube.posX+cube.dx;
-    cube.posY=cube.posY+cube.dy;
+    cube.posX+=cube.dx;
+    cube.posY+=cube.dy;
+    // Gravity
+    if (cube.posY<sol[lvl]-20)
+    {
+        inJump=1;
+        cube.dy+=gravity[lvl];
+    }
+    if (cube.posY>=sol[lvl]-20)
+    {
+        cube.posY=sol[lvl]-20;
+        inJump=0;
+        cube.dy=0;
+    }
 
+}
+
+function jump()
+{
+    if (inJump==0)
+    {
+        inJump=1;
+        cube.dy=-speed[lvl]*baseSpeed*2;
+    }
+    
+    
 }
 
 function drawCube(c2)
 {
     ctx.fillStyle = c2;
-    
-    ctx.fillRect(cube.posX,cube.posY,20,20);
+    if (inJump==0)
+    {
+
+        ctx.fillRect(cube.posX,cube.posY,20,20);
+    }
+    if (inJump==1) 
+    {
+        ctx.fillRect(cube.posX,cube.posY,20,20);
+      
+
+    }
 }
 
+function testArrive()
+{
+    if(cube.posX>=width)    
+    {
+        creation=0;
+        lvl=lvl+1;
+    }
+    if(lvl>=nbLvl)
+    {
+        creation=0;
+        lvl=0;
+    }    
+}
 
 function clearCanvas(ctx) 
 {
     ctx.clearRect(0, 0, width, height)
 }
+
+window.onkeydown = keydownControl;
+
+function keydownControl(e) 
+{
+    switch (e.keyCode) 
+    {
+        case 13:
+            stopTheGame=Math.abs(stopTheGame - 1);
+        break;
+        case 32:
+            jump();
+        break;
+    }
+    if (e.preventDefault) 
+    {
+        e.preventDefault();
+        e.stopPropagation();
+    } 
+    else 
+    {
+        e.returnValue = false;
+        e.cancelBubble = true;
+    }
+    return false;
+    }
 
 
 }
