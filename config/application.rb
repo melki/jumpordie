af0995e406d0c94c9ad5213613d2a7f9e9706823
melki <melchior.toldi@gmail.com>
2013-05-27 22:12:20 +0200

    ajout bloc mobiles + son

--- config/application.rb
+++ config/application.rb
@@ -52,6 +52,7 @@ module Box
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
+    config.assets.paths << "#{Rails.root}/app/assets/audios"
     # Enable the asset pipeline
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
